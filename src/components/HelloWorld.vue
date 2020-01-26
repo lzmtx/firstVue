7d72de123d3ac2ece6c9145567201289ddf8f4fb
lzmtx <3162669269@qq.com>
2020-01-26 16:49:01 +0800

    第一次提交

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>哈哈哈</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: '我的第一个vue应用'
+    }
+  }
+}
+</script>
